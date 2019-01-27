38e7f5102eb5dfef99a708f2cc767cd0e507abdf
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2019-01-27 22:02:50 +0100

    Initial commit

---
+++ src/main/kotlin/com/defaultxyz/skylineapi/SkylineApiApplication.kt
@@ -0,0 +1,11 @@
+package com.defaultxyz.skylineapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkylineApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkylineApiApplication>(*args)
+}
